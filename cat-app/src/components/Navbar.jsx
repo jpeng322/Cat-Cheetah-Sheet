de940303ca9e856878606398ea0c88471b141d9e
Jacky <jackypengsmail@gmail.com>
2023-01-17 02:39:47 -0500

    Updated nav button style

--- cat-app/src/components/Navbar.jsx
+++ cat-app/src/components/Navbar.jsx
@@ -1,18 +1,28 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "../css/navbar.css"
-
+import { useState } from "react";
 const NavComp = () => {
+
+    const [togglerColor, setTogglerColor] = useState({ color: "transparent" })
+
+    function toggleColor() {
+        setTogglerColor({color: "red"})
+        setTimeout(() => {
+            setTogglerColor({ color: "transparent", transition: ".5s" })
+            console.log("asdsadasd")
+        }, 1000)
+    }
     return (
         <nav className="home-nav">
-            <Navbar  fluid expand="lg">
+            <Navbar fluid expand="lg">
                 {/* <Container> */}
                 <Navbar.Brand as={NavLink} to="/">
                     <h3>Cat </h3>
                     <h3>Cheetah-</h3>
                     <h3>Sheet</h3>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle style={togglerColor} onClick={() => toggleColor()} aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={NavLink} to="/">Home</Nav.Link>
